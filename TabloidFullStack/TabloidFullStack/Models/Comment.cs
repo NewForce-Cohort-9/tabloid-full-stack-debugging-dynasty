3fbba866c7189aa0c1d5f77334e5097df04cb89b
Andrew Wells <andrewwells96@gmail.com>
2024-09-18 14:20:36 -0400

    testing posts

---
+++ TabloidFullStack/TabloidFullStack/Models/Comment.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TabloidFullStack.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public int UserProfileId { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateDateTime { get; set; }
+        public UserProfile UserProfile { get; set; }
+        public string CreateDateTimeString => CreateDateTime.ToString("dddd, dd MMMM yyyy");
+    }
+}
